2950ca870ebf4fb6cf3b58af5b71cc42994d6423
herb <herb_711@icloud.com>
2018-04-08 20:47:44 +0800

    model ensemble
    搭建模型融合的读写数据框架
    可以正常运行线性回归模型logistic regression

---
+++ LogisticRegression.py
@@ -0,0 +1,23 @@
+#### Libraries
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn import cross_validation
+import sklearn.preprocessing as preprocessing
+from sklearn import linear_model
+import pandas as pd  
+
+class Regression(object):
+    def __init__(self, X, y):
+        #生成模型
+        #X,y = mini_batch
+        self.clf = linear_model.LogisticRegression(C=1.0, penalty='l1', tol=1e-6)
+        self.clf.fit(X, y)
+
+    def evaluate(self, X, y):
+        #简单评估
+        return cross_validation.cross_val_score(self.clf, X, y, cv=5)
+
+    def predict(self, X):
+        #预测
+        return self.clf.predict(X)
